5bc49f4b3892aa9b6a6f8e64781cc5ed8a384600
minashoukrala <165972928+minashoukrala@users.noreply.github.com>
2024-09-07 15:42:47 -0700

    Backend works
    updated file and testing worked

--- BookstoreBackend/api/urls.py
+++ BookstoreBackend/api/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('products/', views.get_products),
     path('cart/add/', views.add_to_cart),
+    path('product/<int:pk>/update/', views.update_product),
 ]
--- BookstoreBackend/api/views.py
+++ BookstoreBackend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product, CartItem
 from .serializers import ProductSerializer, CartItemSerializer
+from rest_framework import status
 
 @api_view(['GET'])
 def get_products(request):
@@ -16,3 +17,16 @@ def add_to_cart(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+def update_product(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
